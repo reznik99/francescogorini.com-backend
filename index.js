2cee4bcec3696f76a32a7cb26933d4fd775f7ba1
Francesco <goras.francesco@gmail.com>
2021-04-19 01:08:22 +1200

    Tested working endpoint

--- index.js
+++ index.js
@@ -5,22 +5,20 @@ const bodyParser = require('body-parser');
 const express = require('express')
 
 // Express Middleware
+const app = express()
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-const app = express()
 
 // Nodemailer
 let transporter = nodemailer.createTransport({ sendmail: true }) // Auth-less
-
 
 // Routes
 app.get('/', (req, res) => {
     return res.send('Received a GET HTTP method');
 });
 
-
 app.post('/api/sendEmail', async (req, res) => {
-
+    console.log(req.body)
     if (req.body.name && req.body.email && req.body.message) {
         //save variables
         let name = req.body.name;
@@ -43,20 +41,20 @@ app.post('/api/sendEmail', async (req, res) => {
 
             if (recaptcha_json.success) {
                 if (transporter.sendMail($recipient, "Message from ".$name, $msg, $headers)) {
-                    res.statusCode(202) // (Accepted) Email sent!
+                    res.sendStatus(202) // (Accepted) Email sent!
                 } else {
-                    res.statusCode(500) //Error: (Internal Error) Email not sent.
+                    res.sendStatus(500) //Error: (Internal Error) Email not sent.
                 }
             } else {
-                res.statusCode(403) //Error: (Forbidden) Email not sent. Captcha FAILED!
+                res.sendStatus(403) //Error: (Forbidden) Email not sent. Captcha FAILED!
             }
         } else {
-            res.statusCode(403) //Error: (Forbidden) Email not sent. Captcha FAILED!
+            res.sendStatus(403) //Error: (Forbidden) Email not sent. Captcha FAILED!
         }
     }
 });
 
 
 app.listen(process.env.PORT, () =>
-    console.log(`francescogorini.com API listening on port ${process.env.PORT}!`),
+    console.log(`francescogorini.com API listening on port ${process.env.PORT || 80}!`),
 );
